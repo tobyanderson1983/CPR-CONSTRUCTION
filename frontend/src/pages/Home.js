fa45d760c3f7add316dfcc9320271791f75ffce4
Toby Anderson <tobyanderson@gmail.com>
2025-03-19 16:42:04 -0700

    removed useState from Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,7 @@
 //Home.js
 
-import React, { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './css/Home.css';
 
 function Home(){
