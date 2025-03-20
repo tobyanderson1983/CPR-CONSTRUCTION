0ddec79840bc3dc7ee03e07313cc41691f66bd1b
Toby Anderson <tobyanderson@gmail.com>
2025-03-19 17:22:08 -0700

    removed userTopology etc

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.log(err));
 
